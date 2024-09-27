271c3021ae76c3d6311107e6030c780b9b473a9b
FabricioIkehara <fah.hiroshi@gmail.com>
2024-09-27 13:08:19 -0300

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VNY3QAUF.mjs";import"./chunk-YKTTKGLK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ZD4EIW7W.mjs";import"./chunk-YKTTKGLK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
